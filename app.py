f7788b4dbfc8b48af54e44a2257c6a848574b537
9elmo6 <am.elmoghazy@gmail.com>
2024-11-18 17:24:57 -0500

    video_demo

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-#----------------------------------------#
-#Jean-Paul Saliba 
-#Student #: 100741759
-#----------------------------------------#
+
 import cv2
 import time
 
@@ -10,7 +7,6 @@ def detect_objects(frame, cascade, color, object_name, counters):
     
     for (x, y, w, h) in objects:
         cv2.rectangle(frame, (x, y), (x+w, y+h), color, 2)
-        # print(f"Boundary box: {object_name}: {x}, {y}, {w}, {h}")
     
     counters[object_name] += len(objects)
 
@@ -24,7 +20,6 @@ def main():
 
     print("Generated classification objects through online XML files")
 
-    # counts the number of each object detected
     counters = {
         'car': 0,
         'bike': 0,
@@ -33,7 +28,7 @@ def main():
     }
 
     num_frames = 0
-    total_execution_time = 0.0  # Initialize total execution time
+    total_execution_time = 0.0 
 
     for _ in range(500):
         ret, frame = cap.read()
@@ -45,7 +40,6 @@ def main():
 
         start = time.time()
 
-        # detects the objects and draws a rectangle around them
         detect_objects(frame, car_cascade, (0, 0, 255), 'car', counters)
         detect_objects(frame, bikes_cascade, (0, 255, 0), 'bike', counters)
         detect_objects(frame, pedestrian_cascade, (255, 0, 0), 'pedestrian', counters)
@@ -54,8 +48,7 @@ def main():
         end = time.time()
 
         frame_execution_time = end - start
-        total_execution_time += frame_execution_time  # Accumulate frame execution times
-
+        total_execution_time += frame_execution_time 
         print(f"Inference time: {frame_execution_time}")
 
         cv2.imshow('video2', frame)
@@ -65,11 +58,9 @@ def main():
 
     cv2.destroyAllWindows()
 
-    # prints the total number of inferences and the number of each object detected
     print(f"Total inferences: {num_frames}")
     print(f"Cars: {counters['car']} Bikes: {counters['bike']} Pedestrian: {counters['pedestrian']} Bus: {counters['bus']}")
 
-    # prints total execution time and average execution time per frame
     print(f"Total execution time: {total_execution_time} seconds")
     print(f"Average execution time per frame: {total_execution_time / num_frames} seconds")
 
